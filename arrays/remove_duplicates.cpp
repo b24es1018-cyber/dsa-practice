ffe1198135133ed2b2e3e365b00cd3d7c5a36120
Nishant <b24es1018@iitj.ac.in>
2026-06-08 17:26:48 +0530

    arrays:remove duplicates

---
+++ arrays/remove_duplicates.cpp
@@ -0,0 +1,17 @@
+/*
+firstly i think of sets but for optimal i use two pointer 
+tc=O(n)
+sc=O(1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int remove_duplicates(vector<int>& nums){
+  int n=nums.size();
+  int i=0;
+  int (j=1;j<n;j++){
+    if(nums[i]!=nums[j]){
+        i++;
+        nums[i]=nums[j];
+    }
+  } return i+1;
+}
