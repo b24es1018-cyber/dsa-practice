3098617c8120c5626b80da48a1a1d8f933be2f7d
Nishant <b24es1018@iitj.ac.in>
2026-06-10 14:06:27 +0530

    arrays: rotate matrix by 90

---
+++ arrays/rotate_by90.cpp
@@ -0,0 +1,16 @@
+/*
+to rotate first transpose the matrix then reverse each row
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(vector<vector<int>>& matrix){
+    int n=matrix.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+           swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+}
