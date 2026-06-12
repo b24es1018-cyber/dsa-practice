fcd29766e156b646620a11a92dbe1749c0881b9c
Nishant <b24es1018@iitj.ac.in>
2026-06-12 12:54:16 +0530

    binary search: in rotated sorted array

---
+++ binary-search/in_rotated_sorted_array.cpp
@@ -0,0 +1,29 @@
+/*
+first check which half is sorted then check wether target lies in sorted or non sorted half
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int search(vector<int>& nums, int target){
+    int n=nums.size();
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return  mid;
+        
+        if(nums[mid]>=nums[low]){
+            if(nums[low]<=target && target<nums[mid]){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        else{
+           if(nums[mid]<target && target<=nums[high]){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        
+
+    } return -1;
+}
+ 
