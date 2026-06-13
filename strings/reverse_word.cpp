a5a4b964ff3f53fe180a35d03f70c1d9eaf96460
Nishant <b24es1018@iitj.ac.in>
2026-06-13 14:49:15 +0530

    strings: reverse words

---
+++ strings/reverse_word.cpp
@@ -0,0 +1,26 @@
+/*
+reverse only words create vector string
+else start from end choose staring and ending
+point for each word and add one by one adding spaces
+*/
+#incude <bits/stdc++.h>
+using namespace std;
+string reverseWords(string s){
+    int i=s.size()-1;
+    string result="";
+    while(i>=0){
+        if(i>0 && s[i]==' '){
+            i--;
+        }
+        if(i<0) break;
+        int end=i;
+        while(i>=0 && s[i]!=' '){
+            i--;
+        }
+        string word=s.substr(i+1,end-1);
+        if(!result.empty()){
+            result+=" ";
+        }
+        result+=word;
+    } return result;
+}
