8fd98bf37507ff8190ee7ffe983362868a407c32
Nishant <b24es1018@iitj.ac.in>
2026-06-15 23:10:04 +0530

    linkedlist: detect a loop

---
+++ linked-lists/detect_loop.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* temp = head;
+
+        unordered_map<ListNode*, int> nodeMap;
+
+        while (temp != nullptr) {
+            if (nodeMap.find(temp) != nodeMap.end()) {
+                return true;
+            }
+
+            nodeMap[temp] = 1;
+            temp = temp->next;
+        }
+
+        return false;
+    }
+};
