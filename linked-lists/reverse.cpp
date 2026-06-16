5e6162c1bbd380570d0aca008eafc2bbb42dfa32
Nishant <b24es1018@iitj.ac.in>
2026-06-16 10:36:53 +0530

    linked lists: reverse

--- linked-lists/reverse.cpp
+++ linked-lists/reverse.cpp
@@ -1,38 +1,21 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
+/*
+one method
+push all elements in a stack(lifo) then mark temp=head
+temp->val=st.top() temp=temp->next do while(temp!=NULL) return head  
+another is to create front and previous and return prvious 
+*/
+#include <bits/stdc++.h>
+using namespace std;
     ListNode* reverseList(ListNode* head) {
-        // Initialize previous pointer to NULL
         ListNode* prev = NULL;
-
-        // Start from the head of the list
         ListNode* temp = head;
-
-        // Traverse the list
         while (temp != NULL) {
-            // Save the next node
             ListNode* front = temp->next;
 
-            // Reverse the current node's pointer
             temp->next = prev;
-
-            // Move prev to current node
             prev = temp;
-
-            // Move to the next node
             temp = front;
         }
-
-        // Return new head (last node becomes first)
         return prev;
     }
-    };
+    
