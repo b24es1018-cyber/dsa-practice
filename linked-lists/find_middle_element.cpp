fd51c3ddd4183f7ce00de889811bbc02c6ed928a
Nishant <b24es1018@iitj.ac.in>
2026-06-16 10:09:00 +0530

    linked lists: find middle element

--- linked-lists/find_middle_element.cpp
+++ linked-lists/find_middle_element.cpp
@@ -1,33 +1,17 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+create two pointers fast and slow
+move fast two steps and slow one step till fast||fast next react null ptr
+return slow
  */
-class Solution {
-public:
+#include <bits/stdc++.h>
+using namespace std;
     ListNode* middleNode(ListNode* head) {
          ListNode *slow = head; 
     
-     // Initialize the fast pointer to the head.
     ListNode *fast = head; 
-
-    // Traverse the linked list using the
-    // Tortoise and Hare algorithm.
     while (fast != NULL && fast->next != NULL) {
-        // Move slow one step.
         slow = slow->next; 
-         // Move fast two steps.
         fast = fast->next->next; 
     }
-    
-    
-     // Return the slow pointer,
-     // which is now at the middle node.
     return slow; 
     }
-};
